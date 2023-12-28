a546c08e8c1893ca8941cc7414aa418a83a5da1d
samueleffiongjacob <samueleffiong685@gmail.com>
2023-12-28 18:15:57 +0100

    es10

--- advance_6_or_advance_3/index.js
+++ advance_6_or_advance_3/index.js
@@ -20,5 +20,43 @@ entries.flat()
 // flatmap : 
 const flatmap3 = array3.flatMap(creature => creature +  6)
 console.log(flatmap3)
-// flatmap allows us to use the basic flat to index a results to depth of one
+// flatmap allows us to use the basic map functions  to index a results to depth of one
 
+
+// trimstart() and trimend()
+
+const userEmail = '                           bidami@gmail.com'
+const UserEmail1 = 'bonnyjack@gamil.com                        '
+
+console.log(userEmail.trimStart())
+console.log(UserEmail1.trimEnd())
+
+// FromEntries: it transform a list of key value pair's into objects.
+
+const userProfil = [['commaderSam', 23],['derekZlander',40],['HanselWe',50]]
+const obj = Object.fromEntries(userProfil) // Object.FrmEntries  : this is es10
+Object.entries(obj)// es6 giving us the same thing back as in es10
+
+
+/****
+ * try{
+ * }catch{
+ *     }
+*/
+
+try{
+    true + 'hi'
+} catch{
+    console.log('you messed up')
+}
+
+// the above is correct so it would run with out getting to the next line 
+
+// the below would run instead  to throw an error it would return the catch blocks 
+
+try{
+    bob + 'hi'
+} catch (error){
+    console.log('your messed up in the code' + error)
+}
+// note : always  pass a parameter in the catch block to view errors 
